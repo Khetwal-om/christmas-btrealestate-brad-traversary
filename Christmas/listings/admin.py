79c7252d8d726aacd130542254be74e98a587fc5
Khetwal-om <36092598+Khetwal-om@users.noreply.github.com>
2019-12-26 16:48:00 +0530

    06 Customize admin area listings,realtors

--- Christmas/listings/admin.py
+++ Christmas/listings/admin.py
@@ -3,4 +3,13 @@ from .models import Listing
 
 # Register your models here.
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_published','price','list_date','realtor')
+    list_display_links = ('id','title')
+    list_filter = ('realtor',)
+    search_fields = ('title','description','address','city','state','zipcode')
+    list_per_page = 20
+
+
+admin.site.register(Listing,ListingAdmin)
