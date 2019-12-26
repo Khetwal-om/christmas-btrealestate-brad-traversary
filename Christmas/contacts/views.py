8465d49e3b531afa32a345f241b87854fe41d1bf
Khetwal-om <36092598+Khetwal-om@users.noreply.github.com>
2019-12-27 00:50:56 +0530

    19 contact form

--- Christmas/contacts/views.py
+++ Christmas/contacts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from contacts.models import  Contact
 # Create your views here.
-
+from django.contrib import messages
 
 def contact(request):
-    return render(request,'',{})
+    if request.method=='POST':
+        listing_id=request.POST['listing_id']
+        listing=request.POST['listing']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        name=request.POST['name']
+        message=request.POST['message']
+        user_id=request.POST['user_id']
+        realtor_email=request.POST['realtor_email']
+        contact = Contact(listing=listing,listing_id=listing_id,name=name,email=email,phone=phone,message=message,user_id=user_id)
+        contact.save()
+        messages.success(request,'Your request has been submitted , a realtor will be back ')
+
+        return redirect('/listings/'+listing_id)
